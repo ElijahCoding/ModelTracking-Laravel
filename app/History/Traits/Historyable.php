fc0d9d7e89797bbc15fc63cd9f30ef9d29201bca
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-08 13:48:55 +0300

    dump differences

--- app/History/Traits/Historyable.php
+++ app/History/Traits/Historyable.php
@@ -4,15 +4,30 @@ namespace App\History\Traits;
 
 use App\History;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 trait Historyable
 {
     protected static function bootHistoryable()
     {
-        parent::boot();
-
         static::updated(function (Model $model) {
+            dd($model->getChangedColumns($model));
+        });
+    }
 
+    protected function getChangedColumns(Model $model)
+    {
+        return collect(
+            array_diff(
+                $model->getChanges(),
+                $original = $model->getOriginal()
+            )
+        )->map(function ($change, $column) use ($original) {
+            return [
+                'column' => $column,
+                'from' => Arr::get($original, $column),
+                'to' => $change
+            ];
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 Route::get('/', function () {
     $user = \App\User::find(1);
 
-    $user->history()->create([
-        'changed_column' => 'name',
-        'changed_value_from' => 'elijah',
-        'changed_value_to' => 'elijah updated'
+    $user->update([
+        'name' => 'elijah'
     ]);
 });
